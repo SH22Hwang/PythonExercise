398ec5d83329a0b9ce01196b1c3de378e30d068b
SH22Hwang <70620484+SH22Hwang@users.noreply.github.com>
2022-02-15 22:04:18 +0900

    ㅠㅠ

--- P11723.py
+++ P11723.py
@@ -30,31 +30,58 @@ import sys
 #     def empty(self):
 #         self.S.clear()
 
+# def P11723():
+#     input = sys.stdin.readline
+#     # s = []
+#     s = set()
+#     lines = int(input())
+#     cmds = list(list(input().split()) for _ in range(lines))
+#     for command in cmds:
+#         if command[0] == "add":
+#             if command[1] not in s:  # 집합의 특성
+#                 s.add(command[1])
+#         elif command[0] == "remove":
+#             if command[1] in s:
+#                 s.remove(command[1])
+#         elif command[0] == "check":
+#             print(1 if command[1] in s else 0)
+#         elif command[0] == "toggle":
+#             if command[1] in s:
+#                 s.add(command[1])
+#             else:
+#                 s.remove(command[1])
+#         elif command[0] == "all":
+#             s = set([i for i in range(1, 21)])
+#         elif command[0] == "empty":
+#             s = set()
 
 def P11723():
     input = sys.stdin.readline
-    # s = []
-    s = set()
+    s = [False for _ in range(21)]  # 비트마스킹
+
     lines = int(input())
-    cmds = list(list(input().split()) for _ in range(lines))
-    for command in cmds:
+    for _ in range(lines):
+        command = list(input().strip().split())  # strip() 추가(개행문자 제거)
+        if len(command) == 2:
+            command[1] = int(command[1])
+
         if command[0] == "add":
-            if command[1] not in s:  # 집합의 특성
-                s.add(command[1])
+            if not s[command[1]]:  # x가 이미 있는 경우에는 연산을 무시. False일 때만 실행
+                s[command[1]] = True
         elif command[0] == "remove":
-            if command[1] in s:
-                s.remove(command[1])
+            if s[command[1]]:
+                s[command[1]] = False
         elif command[0] == "check":
-            print(1 if command[1] in s else 0)
+            print(1 if s[command[1]] else 0)
         elif command[0] == "toggle":
-            if command[1] in s:
-                s.add(command[1])
+            if s[command[1]]:
+                s[command[1]] = False
             else:
-                s.remove(command[1])
+                s[command[1]] = True
         elif command[0] == "all":
-            s = set([i for i in range(1, 21)])
+            s = [True for _ in range(21)]
         elif command[0] == "empty":
-            s = set()
+            s = [False for _ in range(21)]
 
 
 if __name__ == '__main__':
